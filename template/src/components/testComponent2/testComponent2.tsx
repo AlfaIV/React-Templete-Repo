f4fbbc90110817d2839c1fed85493ed8e88aa2a4
alfaiv <alfaiv@yandex.ru>
2026-01-21 18:41:25 +0000

    feat: add test to test component

--- template/src/components/testComponent2/testComponent2.tsx
+++ template/src/components/testComponent2/testComponent2.tsx
@@ -1,22 +1,25 @@
 import { answerTitle, getDataTitle } from '../../constants/constants';
+import { api } from '../../network/axiosClient';
 import './testComponent2.scss'
 
 const testComponent2 = () => {
-    const buttonOnClick = () => {
-        console.log('click');
-    }
+  const buttonOnClick = () => {
+    console.log('click');
+    const response = api.get('/',);
+    console.log(response);
+  }
 
-    return (
-        <>
-            <div className='testComponent2'>
-                <button className='testComponent2__btn' onClick={buttonOnClick}>
-                    {getDataTitle}
-                </button>
-                <h1 className='testComponent2__h1'>{answerTitle}</h1>
-                <div className='testComponent2__body'></div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='testComponent2'>
+        <button className='testComponent2__btn' onClick={buttonOnClick}>
+          {getDataTitle}
+        </button>
+        <h1 className='testComponent2__h1'>{answerTitle}</h1>
+        <div className='testComponent2__body'></div>
+      </div>
+    </>
+  )
 
 }
 
--- template/src/router/router.tsx
+++ template/src/router/router.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout/layout';
 import TestComponent2 from '../components/testComponent2/testComponent2';
 import TestComponent1 from '../components/testComponent1/testComponent1';
-
 import { baseUrl, test2Url } from '../constants/constants';
 
 const RouterWrapper = () => {
